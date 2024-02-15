9229a17b8c1dbc5276a169fcf101cffa7d49a902
Diyana <da4o96@gmail.com>
2024-02-15 19:08:38 +0100

    Add a note for date formatting.
    LocalDate assumes a 2000 base for years, which means yy is parsed as 20yy. This assumption is probably incorrect in our use case given that the dates are implied to be birthdays and they are far in the future. I linked a stackoverflow questions which explains this and a possible solution.

--- app/src/main/java/assignment/Person.java
+++ app/src/main/java/assignment/Person.java
@@ -3,7 +3,6 @@ package assignment;
 import static java.time.format.DateTimeFormatter.ofPattern;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -29,6 +28,8 @@ public record Person(String name, Gender gender, LocalDate birthday) {
         String[] data = line.split(", ");
         String name = data[0];
         Gender gender = Gender.valueOf(data[1].toUpperCase());
+        // Local date assumes that yy corresponds to 20yy which is probably the wrong assumption in our use case.
+        // See https://stackoverflow.com/questions/38354151/how-to-force-java-time-localdate-to-assume-19th-century-as-yy-year-patternfor
         LocalDate birthday = LocalDate.parse(data[2], ofPattern("dd/MM/yy"));
 
         return new Person(name, gender, birthday);
@@ -40,8 +41,8 @@ public record Person(String name, Gender gender, LocalDate birthday) {
      * If the data is not in the correct format a runtime exception will be thrown.
      *
      * @param fileName of the CSV
-     * @throws IOException if any IO problems occur during reading
      * @return a list of Person
+     * @throws IOException if any IO problems occur during reading
      */
     static List<Person> parsePeopleFromCSV(String fileName) throws IOException {
         List<Person> people = new ArrayList<>();
